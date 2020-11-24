eb7b08354ff552c40a62e65c05888d433cbd8d28
Admin <gabrielffguimaraes@gmail.com>
2020-11-24 16:52:11 -0300

    atualizações

--- agenda-angular/src/environments/environment.ts
+++ agenda-angular/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/contatos'
+  apiUrl: 'http://localhost:8081/api/contatos'
 };
 
 /*
--- agenda-api/src/main/java/com/github/gabrielffguimaraes/agendaapi/rest/UsuarioController.java
+++ agenda-api/src/main/java/com/github/gabrielffguimaraes/agendaapi/rest/UsuarioController.java
@@ -5,10 +5,15 @@ import com.github.gabrielffguimaraes.agendaapi.model.repository.UsuarioRepositor
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/usuarios")
@@ -19,6 +24,12 @@ public class UsuarioController {
 
     @PostMapping
     public Usuario AdicionarUsuario(@RequestBody Usuario usuario){
+        Optional<Usuario> user = this.usuarioRepository.
+                verificarJaCadastrado(usuario.getUsuario());
+
+        if(user.isPresent()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário já cadastrado");
+        }
         return usuarioRepository.save(usuario);
     }
 }
