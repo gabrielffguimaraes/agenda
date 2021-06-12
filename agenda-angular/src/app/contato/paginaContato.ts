f4e3d3f46d83fc274c50ae5aba7ec75f9504202b
gabriel ferreira <gferreira@saojose.br>
2021-06-12 12:19:12 -0300

    final

--- agenda-angular/src/app/contato/paginaContato.ts
+++
@@ -1,8 +0,0 @@
-import {Contato} from './contato';
-
-export class PaginaContato {
-    content: Contato[];
-    totalElements: number;
-    size: number;
-    number: number;
-  }
--- agenda-angular/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://agenda-api-br.herokuapp.com/api/contatos',
-  apiUsuarios: 'https://agenda-api-br.herokuapp.com/api/usuarios',
-  apiOauth: 'https://agenda-api-br.herokuapp.com/oauth/token',
-  clientID: 'contato-application',
-  secretID: '@321'
-};
